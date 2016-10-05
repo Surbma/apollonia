96d0422b94bbeb2932ca48c04bd1992558cfc952
Surbma <peter@surbma.hu>
2016-10-05 16:36:28 +0200

    1.0.0
    - Initial commit.

---
+++ apollonia.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+Plugin Name: Apollonia
+Description: Customizations for the install apollonia.
+Version: 1.0.0
+Author: Surbma
+*/
+
+// Prevent direct access to the plugin
+if ( !defined( 'ABSPATH' ) ) exit( 'Good try! :)' );
+
+function apollonia_enqueue_scripts() {
+	wp_enqueue_script( 'apollonia-scripts', plugins_url( '', __FILE__ ) . '/js/apollonia-min.js', array( 'jquery' ), '', true );
+	wp_enqueue_style( 'apollonia-styles', plugins_url( '', __FILE__ ) . '/css/apollonia.css' );
+}
+add_action( 'wp_enqueue_scripts', 'apollonia_enqueue_scripts', 999 );
