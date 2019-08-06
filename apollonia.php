ca69356803bb4f2d8edf753fe1dacb4bf5eeb40c
Surbma <peter@surbma.hu>
2019-08-06 15:19:42 +0200

    16.1
    - Stílusok hierarchikus meghatározásának beállítása.

--- apollonia.php
+++ apollonia.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Apollonia
 Description: Customizations for the install apollonia.
-Version: 16.0
+Version: 16.1
 Author: Surbma
 */
 
@@ -11,7 +11,7 @@ Author: Surbma
 if ( !defined( 'ABSPATH' ) ) exit( 'Good try! :)' );
 
 function apollonia_enqueue_scripts() {
-	wp_enqueue_script( 'apollonia-scripts', plugins_url( '', __FILE__ ) . '/js/apollonia-min.js', array( 'jquery' ), '16.0', true );
-	wp_enqueue_style( 'apollonia-styles', plugins_url( '', __FILE__ ) . '/css/apollonia.css', array(), '16.0' );
+	wp_enqueue_script( 'apollonia-scripts', plugins_url( '', __FILE__ ) . '/js/apollonia-min.js', array( 'jquery' ), '16.1', true );
+	wp_enqueue_style( 'apollonia-styles', plugins_url( '', __FILE__ ) . '/css/apollonia.css', array(), '16.1' );
 }
 add_action( 'wp_enqueue_scripts', 'apollonia_enqueue_scripts', 999 );
