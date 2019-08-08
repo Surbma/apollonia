e58c79d94d3fb6083aad2ffb53c672593514d74c
Surbma <peter@surbma.hu>
2019-08-08 23:33:37 +0200

    16.9
    - Még további stílus javítás a menünél.

--- apollonia.php
+++ apollonia.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Apollonia
 Description: Customizations for the install apollonia.
-Version: 16.8
+Version: 16.9
 Author: Surbma
 */
 
@@ -11,7 +11,7 @@ Author: Surbma
 if ( !defined( 'ABSPATH' ) ) exit( 'Good try! :)' );
 
 function apollonia_enqueue_scripts() {
-	wp_enqueue_script( 'apollonia-scripts', plugins_url( '', __FILE__ ) . '/js/apollonia-min.js', array( 'jquery' ), '16.8', true );
-	wp_enqueue_style( 'apollonia-styles', plugins_url( '', __FILE__ ) . '/css/apollonia.css', array(), '16.8' );
+	wp_enqueue_script( 'apollonia-scripts', plugins_url( '', __FILE__ ) . '/js/apollonia-min.js', array( 'jquery' ), '16.9', true );
+	wp_enqueue_style( 'apollonia-styles', plugins_url( '', __FILE__ ) . '/css/apollonia.css', array(), '16.9' );
 }
 add_action( 'wp_enqueue_scripts', 'apollonia_enqueue_scripts', 9999 );
