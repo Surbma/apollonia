b23ecec0ef36c2aab0d6331f4ed49de3c0a4683c
Surbma <peter@surbma.hu>
2019-08-24 14:29:09 +0200

    17.0
    - New global styles.
    - New global fonts added.

--- apollonia.php
+++ apollonia.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Apollonia
 Description: Customizations for the install apollonia.
-Version: 16.10
+Version: 17.0
 Author: Surbma
 */
 
@@ -11,7 +11,7 @@ Author: Surbma
 if ( !defined( 'ABSPATH' ) ) exit( 'Good try! :)' );
 
 function apollonia_enqueue_scripts() {
-	wp_enqueue_script( 'apollonia-scripts', plugins_url( '', __FILE__ ) . '/js/apollonia-min.js', array( 'jquery' ), '16.10', true );
-	wp_enqueue_style( 'apollonia-styles', plugins_url( '', __FILE__ ) . '/css/apollonia.css', array(), '16.10' );
+	wp_enqueue_script( 'apollonia-scripts', plugins_url( '', __FILE__ ) . '/js/apollonia-min.js', array( 'jquery' ), '17.0', true );
+	wp_enqueue_style( 'apollonia-styles', plugins_url( '', __FILE__ ) . '/css/apollonia.css', array(), '17.0' );
 }
 add_action( 'wp_enqueue_scripts', 'apollonia_enqueue_scripts', 9999 );
