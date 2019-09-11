ebae502ebdf33382b86da1d91a737f99a8e63e29
Surbma <peter@surbma.hu>
2019-09-11 09:36:26 +0200

    17.1
    - Oldalsáv elválasztó szélességének módosítása.

--- apollonia.php
+++ apollonia.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Apollonia
 Description: Customizations for the install apollonia.
-Version: 17.0
+Version: 17.1
 Author: Surbma
 */
 
@@ -11,7 +11,7 @@ Author: Surbma
 if ( !defined( 'ABSPATH' ) ) exit( 'Good try! :)' );
 
 function apollonia_enqueue_scripts() {
-	wp_enqueue_script( 'apollonia-scripts', plugins_url( '', __FILE__ ) . '/js/apollonia-min.js', array( 'jquery' ), '17.0', true );
-	wp_enqueue_style( 'apollonia-styles', plugins_url( '', __FILE__ ) . '/css/apollonia.css', array(), '17.0' );
+	wp_enqueue_script( 'apollonia-scripts', plugins_url( '', __FILE__ ) . '/js/apollonia-min.js', array( 'jquery' ), '17.1', true );
+	wp_enqueue_style( 'apollonia-styles', plugins_url( '', __FILE__ ) . '/css/apollonia.css', array(), '17.1' );
 }
 add_action( 'wp_enqueue_scripts', 'apollonia_enqueue_scripts', 9999 );
