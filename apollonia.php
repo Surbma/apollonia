80ea09f1e1f730f8e680d671eb4a4733eea9cb34
Surbma <peter@surbma.hu>
2023-02-19 23:23:24 +0100

    17.4
    - Kisebb kód optimalizálások.
    - A .gitignore fájl kiegészítése.
    - Kisebb megjelenítésbeli igazítások.

--- apollonia.php
+++ apollonia.php
@@ -3,15 +3,14 @@
 /*
 Plugin Name: Apollonia
 Description: Customizations for the install apollonia.
-Version: 17.3
+Version: 17.4
 Author: Surbma
 */
 
 // Prevent direct access to the plugin
 if ( !defined( 'ABSPATH' ) ) exit( 'Good try! :)' );
 
-function apollonia_enqueue_scripts() {
-	wp_enqueue_script( 'apollonia-scripts', plugins_url( '', __FILE__ ) . '/js/apollonia-min.js', array( 'jquery' ), '17.1', true );
-	wp_enqueue_style( 'apollonia-styles', plugins_url( '', __FILE__ ) . '/css/apollonia.css', array(), '17.1' );
-}
-add_action( 'wp_enqueue_scripts', 'apollonia_enqueue_scripts', 9999 );
+add_action( 'wp_enqueue_scripts', function() {
+	wp_enqueue_script( 'apollonia-scripts', plugins_url( '', __FILE__ ) . '/js/apollonia-min.js', array( 'jquery' ), '17.4', true );
+	wp_enqueue_style( 'apollonia-styles', plugins_url( '', __FILE__ ) . '/css/apollonia.css', array(), '17.4.4' );
+}, 99999999 );
