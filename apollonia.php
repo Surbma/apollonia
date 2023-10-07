b534014cd6e7dbf23f877e27866cf5badd635d25
Surbma <peter@surbma.hu>
2023-10-07 18:59:10 +0200

    17.7.2
    - A termek-more-link class-hez tartozó stílusok megerősítése.

--- apollonia.php
+++ apollonia.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Apollonia
 Description: Customizations for the install apollonia.
-Version: 17.7.1
+Version: 17.7.2
 Author: Surbma
 */
 
@@ -11,6 +11,6 @@ Author: Surbma
 if ( !defined( 'ABSPATH' ) ) exit( 'Good try! :)' );
 
 add_action( 'wp_enqueue_scripts', function() {
-	wp_enqueue_script( 'apollonia-scripts', plugins_url( '', __FILE__ ) . '/js/apollonia-min.js', array( 'jquery' ), '17.7.1', true );
-	wp_enqueue_style( 'apollonia-styles', plugins_url( '', __FILE__ ) . '/css/apollonia.css', array(), '17.7.1' );
+	wp_enqueue_script( 'apollonia-scripts', plugins_url( '', __FILE__ ) . '/js/apollonia-min.js', array( 'jquery' ), '17.7.2', true );
+	wp_enqueue_style( 'apollonia-styles', plugins_url( '', __FILE__ ) . '/css/apollonia.css', array(), '17.7.2' );
 }, 99999999 );
